e30897ca6d050beecc0206796d8d9ef2d49394b2
victorguillen20 <57498987+victorguillen20@users.noreply.github.com>
2024-07-15 10:17:45 -0500

    creamos nuestra base de datos y una funcion para implementarla en controllers.users.ts

--- Backend/api-node/src/controllers/controllers.users.ts
+++ Backend/api-node/src/controllers/controllers.users.ts
@@ -4,3 +4,14 @@ import { QueryResult } from 'pg'
 import bcryptjs from 'bcrypt'
 
 import { generarCorreoElectronico, obtenerFechaHoraActual, validarIdentificacion, validarParametro, validarPassword, validarUsuario } from '../utils/validaciones';
+
+export const getAllUsers = async (req: Request, res: Response): Promise<Response> => {
+    try {        
+        const response: QueryResult = await pool.query('select*from obtenertodoslosUsuarios()');
+        return res.status(200).json(response.rows);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Internal Server error...')
+    }
+    
+}
