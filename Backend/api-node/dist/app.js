2933f26f2d68cf5c6ef3dc3c99a09b3a91025010
victorguillen20 <57498987+victorguillen20@users.noreply.github.com>
2024-07-15 09:33:42 -0500

    configuramos el archivo app.ts para inicializar el servidor

---
+++ Backend/api-node/dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const app = (0, express_1.default)();
+const corsOptions = {
+    origin: 'http://localhost:4200', // Permitir solo solicitudes desde este origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type'], // Encabezados permitidos
+};
+//midlewares
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: false }));
+app.listen(4000);
+console.log('Server on port', 4000);
---
+++ Backend/api-node/src/app.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors, { CorsOptions } from 'cors';
+
+const app = express();
+
+
+const corsOptions: CorsOptions = {
+    origin: 'http://localhost:4200', // Permitir solo solicitudes desde este origen
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type'], // Encabezados permitidos
+};
+
+//midlewares
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(4000);
+console.log('Server on port', 4000);
