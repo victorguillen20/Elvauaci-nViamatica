792aa2287fd0bc9b602c8bcfbe62e7da6cd9e35a
victorguillen20 <57498987+victorguillen20@users.noreply.github.com>
2024-07-15 10:46:10 -0500

    creamos las funcion para registrar usuarios desde el server

--- Backend/api-node/src/controllers/controllers.users.ts
+++ Backend/api-node/src/controllers/controllers.users.ts
@@ -15,3 +15,52 @@ export const getAllUsers = async (req: Request, res: Response): Promise<Response
     }
     
 }
+
+export const insertUsers = async (req: Request, res: Response): Promise<Response> => {
+    const { username, password, nombres, apellidos, identificacion, fechanacimiento } = req.body;
+    try {
+        //Generamos el correo electronico
+        const correogeneradoPromise = generarCorreoElectronico(nombres, apellidos);
+        const correogenerado = await correogeneradoPromise;
+        console.log(correogenerado);
+        
+        //validamos el usuario
+        const usuariovalidadoPromise = validarUsuario(username);
+        const usuariovalidado = await usuariovalidadoPromise;
+        if (usuariovalidado !== 'true') {
+            console.log(usuariovalidado);
+            return res.status(200).json({ registrodeusuario: usuariovalidado});
+        }
+
+        //validamos el password
+        const passwordvalidado = validarPassword(password);
+        if (passwordvalidado !== 'true') {
+            console.log(passwordvalidado);
+            return res.status(200).json({ registrodeusuario: passwordvalidado});
+        }
+
+        //validamos la identificacion
+        const identificacionvalidada = validarIdentificacion(identificacion);
+        if (identificacionvalidada !== 'true') {
+            console.log(identificacionvalidada);
+            return res.status(200).json({ registrodeusuario: identificacionvalidada});
+        }
+        
+        //codificamos la contrase;a
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
+        
+        //enviamos los datos a registrar
+        const response: QueryResult = await pool.query('select registrarUsuarioEstandar($1, $2, $3, $4, $5, $6, $7)', 
+            [username, hashedPassword,  correogenerado, nombres, apellidos, identificacion, fechanacimiento]);
+        
+        if (response.rows[0].registrarusuarioestandar == false) {
+            return  res.status(500).json({ message: 'Error al cargar los datos'});    
+        }
+        return res.status(200).json({ registrarusuarioestandar: true});               
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json('Internal Server error...')
+    }   
+    
+}
